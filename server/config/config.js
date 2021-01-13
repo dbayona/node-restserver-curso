cee895659c92927cb757e50c50e5982222d4754e
Daniel Bayona <dbayona@credicorpbank.com>
2021-01-13 16:22:52 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,6 @@ process.env.PORT = process.env.PORT || 3000;
 
 // ========================
 // Entorno
-// string: mongodb+srv://cafeusr:UchGhY8BJzJFpAyu@cluster0.n6al9.mongodb.net/cafe?authSource=admin&replicaSet=atlas-o1bkfa-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
 // ========================
 
 //Variable de Entorno de HEROKU
@@ -14,7 +13,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ========================
 // Base de Datos
-// string: mongodb+srv://cafeusr:UchGhY8BJzJFpAyu@cluster0.n6al9.mongodb.net/cafe?authSource=admin&replicaSet=atlas-o1bkfa-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
 // ========================
 
 let urlDB;
@@ -22,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafeusr:UchGhY8BJzJFpAyu@cluster0.n6al9.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
