dbdad08496bd3c914ce259caf38ab92a04c8bc49
Daniel Bayona <dbayona@credicorpbank.com>
2021-01-13 15:49:16 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,26 @@
 // ========================
 
 process.env.PORT = process.env.PORT || 3000;
+
+// ========================
+// Entorno
+// string: mongodb+srv://cafeusr:UchGhY8BJzJFpAyu@cluster0.n6al9.mongodb.net/cafe?authSource=admin&replicaSet=atlas-o1bkfa-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
+// ========================
+
+//Variable de Entorno de HEROKU
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ========================
+// Base de Datos
+// string: mongodb+srv://cafeusr:UchGhY8BJzJFpAyu@cluster0.n6al9.mongodb.net/cafe?authSource=admin&replicaSet=atlas-o1bkfa-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
+// ========================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafeusr:UchGhY8BJzJFpAyu@cluster0.n6al9.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
---
+++ server/routes/usuario.js
@@ -0,0 +1,154 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+
+const app = express();
+
+//Obtener todos los usuarios con paginacion
+app.get('/usuario', function(req, res) {
+
+    let desde = Number(req.query.desde) || 0;
+
+    let limite = Number(req.query.limite) || 5;
+
+    let condicion = { estado: true };
+
+    //Excluyendo campos: se pasa como 2do argumento al metodo find los campos que solo se quieren mostrar
+    Usuario.find(condicion, 'nombre email role estado google img')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            //count -- countDocuments | count esta deprecado
+            Usuario.countDocuments(condicion, (err, conteo) => {
+
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cuantos: conteo
+                });
+
+            });
+        });
+
+    //res.json('get Usuario');
+});
+
+//Crear Usuario
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role,
+        google: body.google
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        //usuarioDB.password = null;
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+    /*if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+    } else {
+        res.json({
+            persona: body
+        });
+    }*/
+});
+
+//Actualizar usuario
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+
+    //Arreglo con los campos que permitire actualizar, por medio de la dependencia: underscore
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    //options --> {new: true} -- devuelve el objeto modificado de la bd
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        //usuarioDB.save
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+//Eliminar usuario
+app.delete('/usuario/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    //Arreglo con los campos que permitire actualizar, por medio de la dependencia: underscore
+    //let body = _.pick(req.body, ['estado']);
+
+    let data = {
+        estado: false
+    };
+
+    //options --> {new: true} -- devuelve el objeto modificado de la bd
+    Usuario.findByIdAndUpdate(id, data, { new: true }, (err, usuarioBorrado) => {
+
+        //Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+
+    });
+
+    //res.json('delete Usuario');
+});
+
+module.exports = app;
